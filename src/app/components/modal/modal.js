d327edb001ebc60bba5dde7670fc8535df7b6b41
Christopher <c.q.ezzard@gmail.com>
2024-11-17 15:57:45 -0700

    login edits to ui

--- src/app/components/modal/modal.js
+++ src/app/components/modal/modal.js
@@ -4,22 +4,46 @@ import styles from "./modal.css";
 
 
 class LoginModal extends Component{
+    constructor(){
+        super();
+        this.username = '';
+        this.password = '';
+    }
+
+    handleSubmit(){
+
+    }
+
+    handleChangeUsername(value){
+        this.username = value;
+    }
+
+    handleChangePassword(value){
+        this.password = value;
+    }
+
     render() {
         return <div id='loginModal' className={styles.loginModal}>
             <form >
                 <div id='header'className={styles}>
                     <h2 className={styles} id='loginHeader'>Account Login</h2><br />
-                    <p>Please enter your username and password to log into your account</p>
+                    <p>Enter your login details below.</p>
                 </div>
-                <div id='inputFields' className={styles}>          
-                    <input placeholder='Username' type='text' id='username'name='username' className={styles.input}></input><br /><br />    
-                    <input placeholder='Password' type='text' id='password'name='password' className={styles.input}></input><br /><br />
-                </div>
+                <div class="form__group field">
+                    <input type="input" class="form__field" placeholder="username" name="username" id='username' required />
+                    <label for="username" class="form__label">Username</label>
+                    <input type="input" class="form__field" placeholder="password" name="password" id='password' required />
+                    <label for="password" class="form__label">Password</label>
+                </div>      
                 <div id='modalFooter' className={styles} >
                     <button id='submitForm' className={styles.button}>Sign In</button>
+                    <p>
+                        <a href="url">Forgot Password?</a>
+                    </p>
                 </div>
             </form>
         </div>
+
       }
 }
 

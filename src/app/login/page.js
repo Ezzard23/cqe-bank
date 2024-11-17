27c773f1ad8391fbcf6b4921c20f3be437ea03ff
Christopher <c.q.ezzard@gmail.com>
2024-11-17 15:58:29 -0700

    static files and css

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,11 +1,11 @@
 import InitalButton from "../components/buttons/buttons";
 import LoginModal from "../components/modal/modal";
+import styles from "./login.css";
 export default function Login() {
     return ( 
-        <div>
+        <div id="loginPage" className={styles.loginPage}>
             <p>Login Page</p>
             <LoginModal />
         </div>
-
     );
 }
